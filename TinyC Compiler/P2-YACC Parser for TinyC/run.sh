3c6d7658f6a078275c428c5da6208cff22c32cc0
Ecolash <codeandcode0@gmail.com>
2024-09-29 08:15:19 +0530

    Minor fix

--- TinyC Compiler/P2-YACC Parser for TinyC/run.sh
+++ TinyC Compiler/P2-YACC Parser for TinyC/run.sh
@@ -10,9 +10,9 @@ flex defs.l
 echo "+++ (3) Compiling the lexer, parser, and main.c..."
 gcc -o parser lex.yy.c defs.tab.c main.c -ll
 
-echo "+++ (4) Running the parser on test.c and storing the output in output.txt..."
-./parser test.c > output.txt
+echo "+++ (4) Running the parser on test.c and storing the output in output.out..."
+./parser test.c > output.out
 
 echo ""
-echo "Done! Output stored in output.txt"
+echo "Done! Output stored in output.out"
 rm -f lex.yy.c defs.tab.c defs.tab.h parser
