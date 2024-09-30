929173e70d6e909e853b4d485ac3b1f1412ba534
Ecolash <codeandcode0@gmail.com>
2024-09-30 22:37:39 +0530

    +1 Test file

--- A4/run.sh
+++ A4/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+echo "+++ Generating parser code with yacc..."
 yacc -d poly.y
+
+echo "+++ Generating lexical analyzer code with flex..."
 flex poly.l
+
+echo "+++ Compiling with gcc..."
 gcc -o parser polyutils.c
-./parser input.txt > output.txt
+
+echo "+++ Running parser on input2.txt..."
+./parser input2.txt > output.txt
+
+echo "+++ Cleaning up generated files..."
 rm -f lex.yy.c y.tab.c y.tab.h parser
+
+echo "+++ Done."
